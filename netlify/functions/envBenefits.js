c526a2ae8dd9022766f3bed04355c144a1b593f6
James Stuckey Weber <james.stuckeyweber@aunalytics.com>
2022-06-20 10:51:11 -0400

    Revert node-fetch to v2 for cjs

--- netlify/functions/envBenefits.js
+++ netlify/functions/envBenefits.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 
 const API_ENDPOINT = `https://monitoringapi.solaredge.com/site/${process.env.SITE}/envBenefits?api_key=${process.env.API_KEY}`;
 
