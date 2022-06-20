1fa2cab850cf172bb8ad5591fb17c71d099e29dc
James Stuckey Weber <james.stuckeyweber@aunalytics.com>
2022-06-19 22:02:07 -0400

    Remove unused

--- netlify/functions/envBenefits.js
+++ netlify/functions/envBenefits.js
@@ -1,10 +1,4 @@
-exports.handler = async function (event, context) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ api: process.env.API_KEY, site: process.env.SITE }),
-    };
-  }
- const fetch = require("node-fetch");
+const fetch = require("node-fetch");
 
 const API_ENDPOINT = `https://monitoringapi.solaredge.com/site/${process.env.SITE}/envBenefits?api_key=${process.env.API_KEY}`;
 
