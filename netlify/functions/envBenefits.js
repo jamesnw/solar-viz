0c6a43d9e5daeab95da71d1c7440c895f80856ce
James Stuckey Weber <james.stuckeyweber@aunalytics.com>
2022-06-19 22:01:46 -0400

    Load env benefits

--- netlify/functions/envBenefits.js
+++ netlify/functions/envBenefits.js
@@ -4,4 +4,16 @@ exports.handler = async function (event, context) {
       body: JSON.stringify({ api: process.env.API_KEY, site: process.env.SITE }),
     };
   }
-  
+ const fetch = require("node-fetch");
+
+const API_ENDPOINT = `https://monitoringapi.solaredge.com/site/${process.env.SITE}/envBenefits?api_key=${process.env.API_KEY}`;
+
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
+    .then((response) => response.json())
+    .then((data) => ({
+      statusCode: 200,
+      body: data,
+    }))
+    .catch((error) => ({ statusCode: 422, body: String(error) }));
+};
