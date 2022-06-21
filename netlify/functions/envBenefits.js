19dd2ea06439a294f110d2a2d19be7453dbc069f
James Stuckey Weber <james.stuckeyweber@aunalytics.com>
2022-06-20 21:04:36 -0400

    Remove header

--- netlify/functions/envBenefits.js
+++ netlify/functions/envBenefits.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 const API_ENDPOINT = `https://monitoringapi.solaredge.com/site/${process.env.SITE}/envBenefits?api_key=${process.env.API_KEY}`;
 
 exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
+  return fetch(API_ENDPOINT)
     .then((response) => response.json())
     .then((data) => ({
       statusCode: 200,
