7e034d91a96bdb68b2e9766680d4db85dcbbf958
James Stuckey Weber <james.stuckeyweber@aunalytics.com>
2022-06-21 21:47:32 -0400

    Add fetch and very bad display

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-module.exports = {};
+const demo = { "envBenefits": { "gasEmissionSaved": { "units": "kg", "co2": 144.7252, "so2": 104.58095, "nox": 33.350616 }, "treesPlanted": 2.4086556, "lightBulbs": 623.8424 } }
+const isDemo = false;
+
+function fetchEnvBenefits() {
+    if (isDemo) {
+        return Promise.resolve(demo);
+    } else {
+        return fetch('.netlify/functions/envBenefits')
+    }
+}
+function makeTrees({treesPlanted}){
+    const el = document.getElementById('trees');
+    const fullTrees = Math.floor(treesPlanted); 
+    el.innerHTML = Array(fullTrees).join('🌳');
+}
+function makeBulbs({lightBulbs}){
+    const el = document.getElementById('bulbs');
+    const fullBulbs = Math.floor(lightBulbs); 
+    el.innerHTML = Array(fullBulbs).join('💡');
+}
+fetchEnvBenefits().then(res=>res.envBenefits).then(res => {
+   makeTrees(res);
+   makeBulbs(res);
+})
+
